8daf4fde307ebe272fecf69d9c291dd124193023
Andy Rossmeissl <andy@rossmeissl.net>
2011-03-10 14:18:40 -0500

    A litle more detail in the controller

--- app/controllers/carbon_footprints_controller.rb
+++ app/controllers/carbon_footprints_controller.rb
@@ -1,6 +1,9 @@
 class CarbonFootprintsController < ApplicationController
+  layout 'thickbox' # Assuming this is how this is handled
+  
   respond_to do |wants|
     wants.js do
+      @title = 'Carbon footprint' # Assuming ivar name
       # These should be pulled from the environment we're looking at.
       @carrier = 'ec2'
       @instance_class = 'm1.large'
